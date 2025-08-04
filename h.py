5aaeb0f4cb6e8eec7a4c2978209fdb1675c56a71
praveenkrishna95 <pgandi@gitam.in>
2025-08-04 15:06:12 +0530

    Update h.py

--- h.py
+++ h.py
@@ -2,7 +2,6 @@ import math
 a = 1
 b = -5
 c = 6
-
 discriminant = b**2 - 4*a*c
 if discriminant >= 0:
     root1 = (-b + math.sqrt(discriminant)) / (2*a)
@@ -10,3 +9,4 @@ if discriminant >= 0:
     print(f"Roots: {root1}, {root2}")
 else:
     print("No real roots")
+//comment//
